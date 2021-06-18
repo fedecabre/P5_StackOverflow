bde2108e9fc312a152292e3a2c1dbb5e3388eee0
fedecabre <fedecabre@gmail.com>
2021-06-18 14:02:34 +0200

    API fonctionnelle avec le modèle keras embedding et nettoyée

--- API/request.py
+++ API/request.py
@@ -4,7 +4,7 @@ import json
 url = 'http://0.0.0.0:5000/api/'
 
 
-text= "How can I r r r r r check if a keyboard modifier is pressed (Shift, Ctrl, or Alt)"
+text= "how install python" #"How can I r r r r r check if a keyboard modifier is pressed (Shift, Ctrl, or Alt)"
 data = [text]
 # data = [input()]
 j_data = json.dumps(data)
